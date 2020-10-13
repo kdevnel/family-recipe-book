11b2581ee18fde5b39e9e8c113aaf85547b1a164
Kyle Nel <kyle@devnel.com>
2020-10-13 16:34:04 +0200

    Updated ACF Fields and recipe ingredients

--- wp-content/plugins/family-recipe-book/public/partials/single-recipe-ingredients.php
+++ wp-content/plugins/family-recipe-book/public/partials/single-recipe-ingredients.php
@@ -9,24 +9,24 @@
  */
 
 // check if the repeater field has rows of data
-if( have_rows('recipe_part') ): ?>
+if (have_rows('recipe_part')): ?>
 
     <div class="recipe-ingredients">
         <h3 id="ingredients" class="ingredients-title">Ingredients</h3>
         <?php
 
         // loop through the rows of data
-        while ( have_rows('recipe_part') ) : the_row(); ?>
+        while (have_rows('recipe_part')) : the_row(); ?>
             <div class="ingredient-part-wrap">
                 <h4 class="ingredient-part-title"><?php the_sub_field('recipe_part_title'); ?></h4>
                 <?php
 
                 //display the recipe ingredients list (sub-repeater)
-                if( have_rows( 'ingredient_list' ) ): ?>
+                if (have_rows('ingredient_list')): ?>
                     <ul class="ingredient-list">
                     <?php
-                    while ( have_rows( 'ingredient_list' ) ) : the_row(); ?>
-                        <li class="ingredient-list-item" itemprop="recipeIngredient"><?php the_sub_field('ingredient'); ?></li>
+                    while (have_rows('ingredient_list')) : the_row(); ?>
+                        <li class="ingredient-list-item" itemprop="recipeIngredient"><?php the_sub_field('quantity'); ?> <?php the_sub_field('measurement'); ?> <?php the_sub_field('ingredient'); ?></li>
                     <?php endwhile; ?>
                     </ul>
                 <?php endif; ?>
@@ -34,7 +34,7 @@ if( have_rows('recipe_part') ): ?>
             </div><!-- .ingredient-part-wrap -->
         <?php endwhile; ?>
     </div>
-<? else : ?>
+<?php else : ?>
     <div>No ingredients listed</div>
 
 <?php endif; ?>
